a60a8a74be703a12938f8d73e4978f83f6710b9f
shafaqueammar <shafaqueammar@gmail.com>
2024-11-14 23:57:30 +0500

    project 43 to 52

---
+++ index.js
@@ -0,0 +1,29 @@
+///////Question 1
+
+// function flower(){
+//     alert("This is a Pink Flower");
+// }
+//////Question 2
+
+// function clicked() {
+//     alert("Thank you for purchasing mobile from us!");
+// }
+
+//////Question 3
+
+// function markButton(element, color) {
+//     element.style.outline = `3px solid ${color}`;  
+// }
+// function resetButton(element) {
+//     element.style.outline = 'none'; 
+// }
+//////////chapter 48 to 52
+/////////Question 1
+
+
+
+function getEmail(e) {
+    e.preventDefault();  
+    var email = document.getElementById("email"); 
+    console.log(email);
+}
